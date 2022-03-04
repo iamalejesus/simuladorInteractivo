22bfef3c95d26d67dd2464778bc4310ec095fb9b
iamale <iamalejandrojesus@gmail.com>
2022-03-04 16:40:14 -0300

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+let nombre = prompt("Ingrese su nombre: ");
+console.log ("Usted a ingresado: " + nombre);
+
+function cotizarPasaje (){ 
+    ticket = parseInt(prompt("Ingrese cuantos tickets desea comprar: ")),
+    console.log (res)
+    const cotizadorPasaje = new Cotizador (destiny, ticket,clase, food)
+    res = cotizadorPasaje.cotizarPasaje()
+}
---
+++ js/variables.js
@@ -0,0 +1,4 @@
+const destiny = 57.000
+let ticket = 0 
+let clase = 0
+const food = 1.500
