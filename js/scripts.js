4fea4e6f10d7531aa7560efdc3130057ef8e4ad5
iamale <iamalejandrojesus@gmail.com>
2022-03-04 17:11:59 -0300

    corrigiendo funciones js

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,30 @@
+const destiny = 57000;
+let ticket = 0;
+let clase = 0;
+const food = 1500;
+
+class Cotizador {
+    constructor (destiny, ticket, clase, food){
+        this.destino = destiny;
+        this.tickets = ticket;
+        this.clase = clase;
+        this.food = food;
+    }
+    cotizarPasaje (){
+        let resultado = this.tickets * this.destino; 
+        return parseFloat(resultado);
+    }
+}
+
 let nombre = prompt("Ingrese su nombre: ");
 console.log ("Usted a ingresado: " + nombre);
 
-function cotizarPasaje (){ 
-    ticket = parseInt(prompt("Ingrese cuantos tickets desea comprar: ")),
-    console.log (res)
-    const cotizadorPasaje = new Cotizador (destiny, ticket,clase, food)
-    res = cotizadorPasaje.cotizarPasaje()
+function cotizarPasaje(){ 
+    ticket = parseInt(prompt("Ingrese cuantos tickets desea comprar: "));
+    
+    const cotizadorPasaje = new Cotizador (destiny, ticket,clase, food);
+    res = cotizadorPasaje.cotizarPasaje();
 }
+
+cotizarPasaje()
+console.log ("El valor total de los pasajes es de:" + " ARS" ,res);
--- js/variables.js
+++
@@ -1,4 +0,0 @@
-const destiny = 57.000
-let ticket = 0 
-let clase = 0
-const food = 1.500
